55db0a068dec8c537f6ad4eea4977deb2bbd6e46
marcinbrzezinski <xbrzezinx@gmail.com>
2017-10-20 13:37:48 +0200

    Dzialania na stalych liczbach

--- kalkulator.c
+++ kalkulator.c
@@ -6,23 +6,33 @@ int mnoz(int a, int b){
 int dodaj(int a, int b){
 	return a + b;
 }
+int odejmij(int a, int b){
+	return a - b;
+}
+int podziel(int a, int b){
+	return a / b;
+}
 
 
 int main()	/* Moze byc zapisane 'int main(void)'	*/
 {
 	int a = 2, b = 6;		/* deklaracja wraz z inicjacja */
 	char wybor;
+	printf("Co chcesz zrobic? Dzielic, dodac, mnozyc, czy odjac?");
 	
 	scanf("%c", &wybor);	/* czytaj z konsoli i zapisz pod wybor */
 	/* if(wyrazenie) (blok jesli wyrazenie prawdziwe)
 	   else (blok jesli wyrazenie falszywe);	*/
 
-	if(wybor == '*'){
+	if(wybor == '*')
 		printf("Wynik mnozenia: %i\n", mnoz(a, b));
-	}else{
-		 printf("Wynik dodawania: %i\n", dodaj(a,b));
-	};/* <= ten srednik jest opcjonalny jesli wystepuje klamra*/
+	else if(wybor == '+')
+		 printf("Wynik dodawania: %i\n", dodaj(a, b));
 
+	else if(wybor == '-')
+		printf("Wynik odejmowania: %i\n", odejmij(a, b));
+	else if(wybor == '/')
+		printf("Wynik dzielenia: %i\n", podziel(a, b));
 	return 0;
 }
 

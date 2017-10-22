86ff0f71c529ae90a6a4c2f1aca9c03d492b894f
marcinbrzezinski <xbrzezinx@gmail.com>
2017-10-22 15:27:29 +0200

    Kalkulator z pętlą

--- kalkulator.c
+++ kalkulator.c
@@ -1,41 +1,58 @@
 #include <stdio.h>
 
 int mnoz(int a, int b){
-	return a*b;
+	printf("Wynik mnozenia: %i\n", a * b);
+	return a * b;
 }
 int dodaj(int a, int b){
+	printf("Wynik dodawania: %i\n", a + b);
 	return a + b;
 }
 int odejmij(int a, int b){
+	printf("Wynik odejmowania: %i\n", a - b);
 	return a - b;
 }
 int podziel(int a, int b){
+	printf("Wynik dzielenia: %i i reszta %i\n", a / b, a % b);
 	return a / b;
 }
+int licz(int a, int b, char wybor){
+	if(wybor == '*'){
+                mnoz(a, b);
+        }else if(wybor == '+'){
+                dodaj(a, b);
+        }else if(wybor == '-'){
+                odejmij(a, b);
+        }else if(wybor == '/'){
+                podziel(a, b);
+        }else {
+        	puts("Nie moge wykonac takiego dzialania!");
+		return 0;
+        }
+        return 1;
+
+
 
 
 int main()	/* Moze byc zapisane 'int main(void)'	*/
 {
-	int a = 2, b = 6;		/* deklaracja wraz z inicjacja */
+
+
+	int a, b;
 	char wybor;
-	printf("Co chcesz zrobic? Dzielic, dodac, mnozyc, czy odjac?");
+	int jeszcze_raz = 1;
 	
-	scanf("%c", &wybor);	/* czytaj z konsoli i zapisz pod wybor */
-	/* if(wyrazenie) (blok jesli wyrazenie prawdziwe)
-	   else (blok jesli wyrazenie falszywe);	*/
 
-	if(wybor == '*')
-		printf("Wynik mnozenia: %i\n", mnoz(a, b));
+	while(jeszcze_raz == 1);
 
-	else if(wybor == '+')
-		 printf("Wynik dodawania: %i\n", dodaj(a, b));
+		printf("Podaj wartosc pierwszego argumentu:"); scanf("%i", &a);
+		printf("Co chcesz zrobic? Dzielic, dodac, mnozyc, czy odjac?"); scanf(" %c", &wybor);   
+		printf("Podaj wartosc drugiego argumentu:"); scanf("%i", &b);
 
-	else if(wybor == '-')
-		printf("Wynik odejmowania: %i\n", odejmij(a, b));
+		licz(a, b, wybor);
 
-	else if(wybor == '/')
-		printf("Wynik dzielenia: %i\n", podziel(a, b));
+		printf("Czy chcesz dalej liczyc? (1 - tak / 0 - nie) ");
+		scanf("%d", &jeszcze_raz);
+	}
 	return 0;
 }
-
-
